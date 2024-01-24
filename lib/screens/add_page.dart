d31339b9feb46640e76b61c3d00d11fb9f74622a
Dhananjay Hareesh <dhananjay.hareesh@gmail.com>
2024-01-24 21:43:43 +0530

    refactored and completed

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
+import 'package:rest_todo/services/todo_services.dart';
+import 'package:rest_todo/utils/snackbar_helper.dart';
 
 class AddTodoPage extends StatefulWidget {
-  const AddTodoPage({super.key});
+  final Map? todo;
+  const AddTodoPage({
+    super.key,
+    this.todo,
+  });
 
   @override
   State<AddTodoPage> createState() => _AddTodoPageState();
@@ -11,12 +17,27 @@ class AddTodoPage extends StatefulWidget {
 class _AddTodoPageState extends State<AddTodoPage> {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+  bool isEdit = false;
+
+  @override
+  void initState() {
+    super.initState();
+    final todo = widget.todo;
+    if (todo != null) {
+      isEdit = true;
+      final title = todo['title'];
+      final description = todo['description'];
+      titleController.text = title;
+      descriptionController.text = description;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Add Todo'),
+        title: Text(isEdit ? 'Edit Todo' : 'Add Todo'),
       ),
       body: ListView(
         padding: const EdgeInsets.all(20),
@@ -38,23 +59,60 @@ class _AddTodoPageState extends State<AddTodoPage> {
           const SizedBox(
             height: 20,
           ),
-          ElevatedButton(onPressed: () {}, child: const Text('Submit'))
+          ElevatedButton(
+              onPressed: isEdit ? updateData : submitData,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(isEdit ? 'Update' : 'Submit'),
+              ))
         ],
       ),
     );
   }
 
-  void submitData() {
+  Future<void> updateData() async {
     //get data from form
+    final todo = widget.todo;
+    if (todo == null) {
+      print('you cannot call update without todo data');
+      return;
+    }
+    final id = todo['_id'];
+
+    //submit update data to server
+    final isSuccess = await TodoService.updateTodo(id, body);
+
+    //show success or fail message on status
+    if (isSuccess) {
+      showSuccessMessage(context, message: 'Creation success');
+    } else {
+      showErrorMessage(context, message: 'Creation Failed');
+    }
+  }
+
+  Future<void> submitData() async {
+    //submit data to server
+
+    final isSuccess = await TodoService.addTodo(body);
+
+    //show success or fail message on status
+    if (isSuccess) {
+      titleController.text = '';
+      descriptionController.text = '';
+      showSuccessMessage(context, message: 'Creation success');
+    } else {
+      showErrorMessage(context, message: 'Creation Failed');
+    }
+  }
+
+  Map get body {
+    //Get the data from the form
     final title = titleController.text;
     final description = descriptionController.text;
-    final body = {
+    return {
       "title": title,
       "description": description,
       "is_completed": false,
     };
-
-    //submit data ti server
-    //show success or fail message on status
   }
 }
--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rest_todo/screens/add_page.dart';
+import 'package:rest_todo/services/todo_services.dart';
+import 'package:rest_todo/utils/snackbar_helper.dart';
+import 'package:rest_todo/widgets/toto_card.dart';
 
 class TOdoListPage extends StatefulWidget {
   const TOdoListPage({super.key});
@@ -9,22 +12,104 @@ class TOdoListPage extends StatefulWidget {
 }
 
 class _TOdoListPageState extends State<TOdoListPage> {
+  bool isLoading = true;
+  List items = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Todo List'),
+        title: const Text('Todo List'),
+      ),
+      body: Visibility(
+        visible: isLoading,
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: Visibility(
+            visible: items.isNotEmpty,
+            replacement: Center(
+              child: Text(
+                'No Todo items',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ),
+            child: ListView.builder(
+                itemCount: items.length,
+                padding: EdgeInsets.all(8),
+                itemBuilder: (context, index) {
+                  final item = items[index] as Map;
+
+                  return TodoCard(
+                      index: index,
+                      item: item,
+                      navigateEdit: navigateToEditPage,
+                      deleteById: deleteById);
+                }),
+          ),
+        ),
+        child: const Center(
+          child: CircularProgressIndicator(),
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-          onPressed: navigateToAddPage, label: Text('Add Todo')),
+          onPressed: navigateToAddPage, label: const Text('Add Todo')),
     );
   }
 
-  void navigateToAddPage() {
+  Future<void> navigateToAddPage() async {
     final route = MaterialPageRoute(
-      builder: (context) => AddTodoPage(),
+      builder: (context) => const AddTodoPage(),
     );
-    Navigator.push(context, route);
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  Future<void> navigateToEditPage(Map item) async {
+    final route = MaterialPageRoute(
+      builder: (context) => AddTodoPage(todo: item),
+    );
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  Future<void> deleteById(String id) async {
+    //delete the item
+    final isSuccess = await TodoService.deleteById(id);
+    if (isSuccess) {
+      final filtered = items.where((element) => element['_id'] != id).toList();
+      setState(() {
+        items = filtered;
+      });
+      //remove item from list
+    } else {
+      //show error
+      showErrorMessage(context, message: 'Deletion Failed');
+    }
+  }
+
+  Future<void> fetchTodo() async {
+    final response = await TodoService.fetchTodo();
+    if (response != null) {
+      setState(() {
+        items = response;
+      });
+    } else {
+      showErrorMessage(context, message: 'Something went wrong');
+    }
+    setState(() {
+      isLoading = false;
+    });
   }
 }
